4126c9bde48ca7e2d33386f678bbd7513115e503
Durkadevi Iyappan <durkadeviiyappan@gmail.com>
2026-08-26 17:15:38 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        //base case
+        if(s.length() != t.length()){
+            return false;
+        }
+        Map<Character,Integer> map = new HashMap<>();
+        
+        for(int i=0 ; i<s.length() ; i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i) , 0)+1);
+        }
+        for(int i=0 ; i<t.length() ; i++){
+            if(map.containsKey(t.charAt(i))){
+                  map.put(t.charAt(i) , map.get(t.charAt(i))-1);
+            }
+            else{
+              return false;
+            }
+        }
+         for (int val : map.values()) {
+            if (val != 0) 
+            return false;
+         }
+         return true;
+
+     
+    }
+}
