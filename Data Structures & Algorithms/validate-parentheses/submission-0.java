c1606071ded59d4e5f287be06c93aacfcb87ff31
Durkadevi Iyappan <durkadeviiyappan@gmail.com>
2026-09-12 20:18:10 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,30 @@
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        
+        int i=0;
+        while(i < s.length()){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '[' || ch == '{'){
+                st.push(ch);
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                else if((ch == ')' && st.peek() == '(') || (ch == ']' && st.peek() == '[') || (ch == '}' && st.peek() == '{') ){
+                st.pop();
+            }
+            else{
+                return false;
+            }
+         }
+            i++;
+        }
+      
+        
+       return st.isEmpty();
+        
+    }
+}
